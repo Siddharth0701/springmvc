bdc8919f3ace7acbef223b8f736da19bb09b9dac
Siddharth0701 <siddharth.singh0701@gmail.com>
2023-01-03 13:17:38 +0530

    2nd ways to send the data model to view using model

--- src/main/java/springwebmvc/controller/HomeController.java
+++ src/main/java/springwebmvc/controller/HomeController.java
@@ -1,17 +1,30 @@
 package springwebmvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	@RequestMapping("/home")
-	public String home() {
+	@RequestMapping("/")
+	public String home(Model model) {
 		System.out.println("this is home url");
+		model.addAttribute("name", "Siddharth Kumar Singh");
+		model.addAttribute("id", 8070);
+		List<String> friends=new ArrayList<String>();
+		friends.add("Harsh");
+		friends.add("Mohan");
+		friends.add("Sohan");
+		friends.add("uttam");
+		model.addAttribute("f",friends);
+		
 		
 		return "index";
 	}
-	@RequestMapping("/")
+	@RequestMapping("/about")
 	public String about() {
 		System.out.println("this is about page");
 		return "about";
--- src/main/webapp/WEB-INF/view/index.jsp
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<%@page import="java.util.List"%>
 <html>
 <body>
 <h2>Hello World! Home page</h2>
@@ -6,5 +7,24 @@
 <hr>
 <a href="showForm">Hello World form</a>
 <a href="about">About page</a>
+<%
+  String name= (String) request.getAttribute("name");
+Integer id= (Integer) request.getAttribute("id");
+List<String> friends=(List<String>) request.getAttribute("f");
+%>
+<h1>
+   Name is <%= name %>
+   <br>
+Id is <%= id %>
+</h1>
+
+ <%
+ for(String s:friends){
+ %>
+	<h1><%= s %></h1>
+<% 
+ }
+ %>
+
 </body>
 </html>
